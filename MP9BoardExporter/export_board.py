b77f02d27f451451b2f14c4b27d887c8ef49759c
HBroHei <hammerbrohei@gmail.com>
2024-02-18 21:09:46 +0800

    New JSON format for exporting
    Changed from storing string attrbutes to actual JSON attributes. (Basically revamped how data are added to the return variable)

--- MP9BoardExporter/export_board.py
+++ MP9BoardExporter/export_board.py
@@ -1,37 +1,51 @@
-﻿__author__ = "HBroHei"
-__version__ = "0.0.1"
+﻿__author__ = "HammerBroHei"
+__version__ = "1.0.0"
 
-#soopercool101.github.io
+# Ref / docs: soopercool101.github.io/BrawlCrate
 
 from BrawlCrate.API import *
 from BrawlLib.SSBB.ResourceNodes import *
 from BrawlLib.SSBB.Types import *
 from System.IO import *
 
-nodeList = {}
+nodeList = {"Nodes" : {}}
 
-nodeJsonStr = "\"Nodes\":{"
 range_x = [0,0]
 range_y = [0,0]
 range_z = [0,0]
 
+def printDebug(msg):
+    BrawlAPI.ShowMessage(str(msg),"DEBUG")
+
+def printMsg(msg,title="Message"):
+    BrawlAPI.ShowMessage(str(msg),title)
+
+def processAttribute(user_data_class):
+    return_val = {}
+    return_val[user_data_class.Name] = user_data_class.Entries if len(user_data_class.Entries)>1 else user_data_class.Entries[0]
+    # if len(user_data_class.Entries)>1:
+    #     return_val += '{' + ', '.join(user_data_class.Entries) + '}'
+    # else:
+    #     return_val += "\"" + user_data_class.Entries[0] + "\","
+ 
+    return return_val
+
 def openChildNode(rNode,isRecursed = False):
-    global nodeJsonStr
-    if len(rNode.Children)!=0:
-        nodeJsonStr += "{"
+    returnDict = {}
+    if len(rNode.Children)>0:
         for cNode in rNode.Children:
-            nodeJsonStr += "\"" + cNode.Name + "\" :"
-            openChildNode(cNode,True) 
-            nodeJsonStr += ",\n"
-        nodeJsonStr += "\"__dummy__\":\"\""
-        
-        nodeJsonStr += "}"
+            # Loop and add sub nodes
+            returnDict[cNode.Name] = openChildNode(cNode,True) 
+
+        return returnDict
     else:
-        nodeJsonStr += "\""
         for prop in rNode.UserEntries:
-            nodeJsonStr += str(prop) + ";"
-        nodeJsonStr += "x:" + str(rNode.Translation._x) + ";y:" + str(rNode.Translation._y) + ";z:" + str(rNode.Translation._z) + ";"
-        nodeJsonStr += "\""
+            returnDict[prop.Name] = list(prop.Entries) if len(prop.Entries)>1 else prop.Entries[0]
+            
+        returnDict["x"] = str(rNode.Translation._x)
+        returnDict["y"] = str(rNode.Translation._y)
+        returnDict["z"] = str(rNode.Translation._z)
+
         # Check for min/max
         if rNode.Translation._x < range_x[0]:
             range_x[0] = rNode.Translation._x
@@ -46,40 +60,42 @@ def openChildNode(rNode,isRecursed = False):
         elif rNode.Translation._z > range_z[1]:
             range_z[1] = rNode.Translation._z
 
-try:
-    selectedNodeBones = BrawlAPI.SelectedNode.AllBones  # https://soopercool101.github.io/BrawlCrate/class_brawl_lib_1_1_s_s_b_b_1_1_resource_nodes_1_1_m_d_l0_bone_node.html
-except:
-    BrawlAPI.ShowError("Error: make sure you have selected the mdl0 node","Message")
-    exit()
-selectedNodeFirstBones = BrawlAPI.SelectedNode.BoneList
+        return returnDict
 
-listUserEntries = ""
-i = 0
+def exportNode(mdl0Node):
+    finalDict = {"Nodes":[]}
+    # Test if the node is a valid MDL0 node
+    try:
+        selectedNodeBones = mdl0Node.AllBones  # https://soopercool101.github.io/BrawlCrate/class_brawl_lib_1_1_s_s_b_b_1_1_resource_nodes_1_1_m_d_l0_bone_node.html
+    except:
+        BrawlAPI.ShowError("Error: make sure you have selected the mdl0 node","Message")
+        exit()
+    selectedNodeFirstBones = mdl0Node.BoneList
+
+    for ele in selectedNodeFirstBones:
+        finalDict["Nodes"].append({ele.Name : openChildNode(ele)})
+
+    finalDict["max_x"] = range_x[1]
+    finalDict["min_x"] = range_x[0]
+    finalDict["max_y"] = range_y[1]
+    finalDict["min_y"] = range_y[0]
+    finalDict["max_z"] = range_z[1]
+    finalDict["min_z"] = range_z[0]
+
+    # Write the JSON to file
+    fUEdata = open(exportLoc + "\\" + str(mdl0Node)+".json","w")
+    fUEdata.write(str(finalDict).replace("'","\""))
+    fUEdata.close()
+
+    printMsg("File Exported to: " + exportLoc + "\\" + str(mdl0Node)+".json")
+
+##### MAIN CODE #####
 
 exportLoc = BrawlAPI.OpenFolderDialog("Select export location")
 
-for ele in selectedNodeFirstBones:
-    nodeJsonStr += "\"" + ele.Name + "\" : "
-    openChildNode(ele)
-
-'''
-for boneNode in selectedNodeBones:
-    listUserEntries += boneNode.Name + ":\""
-    nodeList[boneNode.Name] = []
-    for prop in boneNode.UserEntries:
-        listUserEntries += str(prop) + ";"
-    listUserEntries += "\",\n"
-'''
-
-nodeJsonStr += "},\"max_x\":" + str(range_x[1])
-nodeJsonStr += ",\"min_x\":" + str(range_x[0])
-nodeJsonStr += ",\"max_y\":" + str(range_y[1])
-nodeJsonStr += ",\"min_y\":" + str(range_y[0])
-nodeJsonStr += ",\"max_z\":" + str(range_z[1])
-nodeJsonStr += ",\"min_z\":" + str(range_z[0])
-
-fUEdata = open(exportLoc + "\\" + str(BrawlAPI.SelectedNode)+".json","w")
-fUEdata.write("{"+nodeJsonStr+"}")
-fUEdata.close()
-
-BrawlAPI.ShowMessage("File Exported","Message")
+# Check if the node is the root node. If yes, iterate through every mdl0 nodes
+if isinstance(BrawlAPI.SelectedNode,BRRESNode):
+    for mdl0Node in BrawlAPI.SelectedNode.FindChildrenByType("./",ResourceType.MDL0):
+        exportNode(mdl0Node)
+else:
+    exportNode(BrawlAPI.SelectedNode)
