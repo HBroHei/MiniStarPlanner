a36b10f6e1859aadc3cca3852d53fded6c9307ff
HBroHei <hammerbrohei@gmail.com>
2024-03-17 18:20:07 +0800

    Fixing image not loaded attempt number 5
    Hopefully the final one

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const canLoadImages = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,2
 showLoading();
 for(const canLoadImg of canLoadImages){
     spacesImgs[canLoadImg] = new Image();
-    spacesImgs[canLoadImg].src = "/icons/" + canLoadImg + ".png";
+    spacesImgs[canLoadImg].src = "./icons/" + canLoadImg + ".png";
     spacesImgs[canLoadImg].onload = () => {
         loadedImgs++;
         console.log("Loaded " + loadedImgs + " images")
